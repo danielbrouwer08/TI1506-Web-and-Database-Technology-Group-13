4159181a4321197653979336004d67961a3126cc
danielbrouwer08 <danielbrouwer08@hotmail.com>
2015-11-18 16:54:16 +0100

    constructor aanmaken

--- src/app.js
+++ src/app.js
@@ -1,3 +1,12 @@
-/**
- * 
- */
+var main = function () {
+	"use strict";
+
+	function ToDo(description,dueDate,priority,reminderDate){
+		this.description = description;
+		this.priority = priority;
+		this.reminderDate = reminderDate;
+	}
+
+
+
+};
