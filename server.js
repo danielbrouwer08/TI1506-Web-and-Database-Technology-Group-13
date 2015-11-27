161325c3f96ee01557ff794c0af8173cee3526da
danielbrouwer08 <danielbrouwer08@hotmail.com>
2015-11-27 14:23:08 +0100

    mooie dingen

--- server.js
+++ server.js
@@ -6,16 +6,28 @@ var server;
 var port = 8000;
 server = express();
 http.createServer(server).listen(port);
+server.use(express.static('static'));
 
-
-
-server.get("/greetme",function(req,res){
-	var query = url.parse(req.url,true).query;
-	var name = ( query["name"]!=undefined) ?
-	query["name"] : "Anonymous";
-	res.send("Greetings "+name);
+server.get("/app",function(req,res){
+	
+	res.sendfile("app.html");
+	
 });
 
-server.get("/goodbye",function(req,res){
-	res.send("Goodbye you!");
+server.get("/splash",function(req,res){
+	
+	res.sendfile("splash.html");
+	
 });
+
+
+// server.get("/greetme",function(req,res){
+// 	var query = url.parse(req.url,true).query;
+// 	var name = ( query["name"]!=undefined) ?
+// 	query["name"] : "Anonymous";
+// 	res.send("Greetings "+name);
+// });
+
+// server.get("/goodbye",function(req,res){
+// 	res.send("Goodbye you!");
+// });
