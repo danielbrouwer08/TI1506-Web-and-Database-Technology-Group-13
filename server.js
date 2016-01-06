e46f47ee2f784f8c01226b904fef9781f2f17ac6
danielbrouwer08 <danielbrouwer08@hotmail.com>
2016-01-06 10:44:57 +0100

    no message

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ var connection = todoAction.connection;
 //locate views for ejs
 server.set('views', __dirname + '/views');
 server.set('view engine', 'ejs');
-server.use(session({secret: 'secretsession'}));
+server.use(session({ secret: 'secretsession' }));
 server.use(passport.initialize());
 server.use(passport.session());
 server.use(bodyparser.urlencoded({ extended: true }));
@@ -37,7 +37,7 @@ server.use(bodyparser.json());
 server.use(express.static('static'));
 
 
-pages.app(server,passport);
+pages.app(server, passport);
 pages.splash(server);
 pages.dashboard(server);
 todoAction.save(server);
@@ -47,40 +47,52 @@ passport.use(new FacebookStrategy({
     clientSecret: fb.appSecret,
     callbackURL: fb.callbackUrl,
     enableProof: false
-  },
-  function(accessToken, refreshToken, profile, done) {
-	var temp = new fbUser(profile.id,profile.displayName);
-    done(null,temp);
-  }
-));
+},
+	function (accessToken, refreshToken, profile, done) {
+		var temp = new fbUser(profile.id, profile.displayName);
+		done(null, temp);
+	}
+	));
 
 server.get('/login/facebook', passport.authenticate('facebook'));
 
 server.get('/login/facebook/callback',
-  passport.authenticate('facebook', { failureRedirect: '/splash' }),
-  function(req, res) {
-    // Successful authentication, redirect home.
-	//console.log(req.user.facebookId);
-    res.redirect('/app');
-  });
+	passport.authenticate('facebook', { failureRedirect: '/splash' }),
+	function (req, res) {
+		// var query = "INSERT INTO user (Id,name) VALUES('" + req.user.facebookId + "','" + req.user.displayName + "')";
+		// connection.query(query, function (error, results, fiels) {
+		// 	console.log(error);
+		// });
+		// query = "INSERT INTO todolist (Id,Owner) VALUES('" + 1 + "','" + req.user.facebookId + "')";
+		// connection.query(query, function (error, results, fiels) {
+		// 	console.log(error);
+		// });
+		
+		// Successful authentication, redirect home.
+		//console.log(req.user.facebookId);
+		res.redirect('/app');
+	});
 
-passport.serializeUser(function(user, done) {
-  done(null, user);
+passport.serializeUser(function (user, done) {
+	done(null, user);
 });
 
-passport.deserializeUser(function(user, done) {
-  done(null, user);
+passport.deserializeUser(function (user, done) {
+	done(null, user);
 });
 
   
-  //Send all todo's from the database to the client
+//Send all todo's from the database to the client
 server.get("/getTodo", function (req, res) {
 	var user = req.user;
 	console.log("user");
 	console.log(user.facebookId);
-	console.log("req:");
-	console.log(req);
-	connection.query('Select * From todoitem WHERE todoitem.DueDate is not null and todoitem.CompletionDate is not null', function (error, results, fiels) {
+	//console.log("req:");
+	//console.log(req);
+	//var query = 'Select * From todoitem WHERE todoitem.DueDate is not null and todoitem.CompletionDate is not null';
+	var query = 'Select * From todoitem WHERE todoitem.ItemOwner=' + req.user.facebookId + ' and todoitem.DueDate is not null and todoitem.CompletionDate is not null';
+
+	connection.query(query, function (error, results, fiels) {
 		console.log(error); //log any errors
 		ToDoArray = [];
 		
@@ -100,44 +112,49 @@ server.get("/getTodo", function (req, res) {
 		//Send the whole ToDoArray to the client
 		res.json(ToDoArray);
 	});
-	
+
 });
 
 //Client sends ToDo to server that has to be saved/altered in the database
 server.post("/saveTodo", function (req, res) {
 	var temptodo = req.body;
-	var id = req.user.facebookId;
-	
+	//var id = req.user.facebookId;
+
 	console.log("The following ToDo has been posted to the server:");
 	console.log(temptodo);
 
-	connection.query('Select * From todoitem WHERE todoitem.DueDate is not null and todoitem.CompletionDate is not null and todoitem.id=' + temptodo.id, function (error, results, fiels) {
-		console.log(error); //log any errors
+	// connection.query('Select * From todoitem WHERE todoitem.DueDate is not null and todoitem.CompletionDate is not null and todoitem.id=' + temptodo.id, function (error, results, fiels) {
+	// 	console.log(error); //log any errors
 		
-		if (results[0] == undefined) {
-			console.log("Todo not in the list; adding it now");
-		}else
-		{
-			console.log("Todo already in the list; altering it now");
-		}
+	// 	if (results[0] == undefined) {
+	// 		console.log("Todo not in the list; adding it now");
+	// 	}else
+	// 	{
+	// 		console.log("Todo already in the list; altering it now");
+	// 	}
 
-		//Alter date format so it will fit the database
-		var temp1 = temptodo.dueDate.split("-")
-		var temp2 = temptodo.reminderDate.split("-")
-		var dueDate = temp1[2] + "-" + temp1[1] + "-" + temp1[0]; 
-		var reminderDate = temp2[2] + "-" + temp2[1] + "-" + temp2[0];
+	//Alter date format so it will fit the database
+	var temp1 = temptodo.dueDate.split("-")
+	var temp2 = temptodo.reminderDate.split("-")
+	var dueDate = temp1[2] + "-" + temp1[1] + "-" + temp1[0];
+	var reminderDate = temp2[2] + "-" + temp2[1] + "-" + temp2[0];
 
-		//Delete all todoItems with received ID
-		var delquery = "DELETE FROM todoItem where Id=" + temptodo.id
-		connection.query(delquery, function (error, results, fiels) {
-			console.log(error);
-		});
+	//Delete all todoItems with received ID
+	//var delquery = "DELETE FROM todoItem where Id=" + temptodo.id
+	var delquery = 'DELETE FROM todoitem where todoitem.id=' + temptodo.id + ' and todoitem.ItemOwner=' + req.user.facebookId;
+	connection.query(delquery, function (error, results, fiels) {
+		console.log("delquery errors:");
+		console.log(error);
+	});
 
-		//Add the received todo with received ID to the database
-		var query = "INSERT INTO todoitem (Id,Title,Text,DueDate,CompletionDate,Priority,Completed,ToDoListID) VALUES('" + temptodo.id + "','" + temptodo.subject + "','" + temptodo.extraInfo + "','" + dueDate + "','" + reminderDate + "','" + temptodo.priority + "','" + temptodo.done + "','" + 1 + "')";
-		connection.query(query, function (error, results, fiels) {
-			console.log(error);
-		});
+	//Add the received todo with received ID to the database
+	//var query = "INSERT INTO todoitem (Id,Title,Text,DueDate,CompletionDate,Priority,Completed,ToDoListID) VALUES('" + temptodo.id + "','" + temptodo.subject + "','" + temptodo.extraInfo + "','" + dueDate + "','" + reminderDate + "','" + temptodo.priority + "','" + temptodo.done + "','" + 1 + "')";
+	var query = "INSERT INTO todoitem (Id,Title,Text,DueDate,CompletionDate,Priority,Completed,ToDoListID,ItemOwner) VALUES('" + temptodo.id + "','" + temptodo.subject + "','" + temptodo.extraInfo + "','" + dueDate + "','" + reminderDate + "','" + temptodo.priority + "','" + temptodo.done + "','" + 1 + "','" + req.user.facebookId + "')";
+	console.log("query: " + query);
+	connection.query(query, function (error, results, fiels) {
+		console.log("query errors:");
+		console.log(error);
+
 	});
 });
 
@@ -147,8 +164,8 @@ server.post("/saveTodo", function (req, res) {
 server.post("/deleteTodo", function (req, res) {
 	var temptodo = req.body;
 	console.log("Deleting todo from server");
-	
-	var query = "DELETE FROM todoItem where Id=" + temptodo.id
+
+	var query = 'DELETE FROM todoitem where todoitem.id=' + temptodo.id + ' and todoitem.ItemOwner=' + req.user.facebookId;
 	connection.query(query, function (error, results, fiels) {
 		console.log(error); //log any errors
 	});
@@ -156,7 +173,7 @@ server.post("/deleteTodo", function (req, res) {
 });
 
 
-//Create an account on the server
+//Create an account on the server (NOT USED ANYMORE)
 server.post("/createaccount", function (req, res) {
 	console.log("Userdata has been posted");
 
@@ -184,29 +201,32 @@ function ToDo(subject, extraInfo, dueDate, priority, reminderDate, id) {
 }
 
 //Constructor for user
-function fbUser(facebookId,displayName)
-{
+function fbUser(facebookId, displayName) {
 	this.facebookId = facebookId;
 	this.displayName = displayName;
 }
 
-
+// function pad(n, width, z) {
+//   z = z || '0';
+//   n = n + '';
+//   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+// }
 
 //SQL Queries
 
-query.todosAmount(server,connection,ejs); 
-query.getTodoList(server,connection,ejs);
-query.getTodoItems(server,connection,ejs); 
-query.get5TodoItems(server,connection,ejs);
-query.getTodoItemsFilterd(server,connection,ejs);
-query.getSubItems(server,connection,ejs);
-query.getTags(server,connection,ejs);
-query.todoListByTag(server,connection,ejs);
-query.totalPendingAndCompleted(server,connection,ejs);
-query.todosCompletedEachWeek(server,connection,ejs);
-query.tagsFrequency(server,connection,ejs);
-query.averageCompletionTime(server,connection,ejs);
-query.lowerThenAverageCompletionTime(server,connection,ejs);
+query.todosAmount(server, connection, ejs);
+query.getTodoList(server, connection, ejs);
+query.getTodoItems(server, connection, ejs);
+query.get5TodoItems(server, connection, ejs);
+query.getTodoItemsFilterd(server, connection, ejs);
+query.getSubItems(server, connection, ejs);
+query.getTags(server, connection, ejs);
+query.todoListByTag(server, connection, ejs);
+query.totalPendingAndCompleted(server, connection, ejs);
+query.todosCompletedEachWeek(server, connection, ejs);
+query.tagsFrequency(server, connection, ejs);
+query.averageCompletionTime(server, connection, ejs);
+query.lowerThenAverageCompletionTime(server, connection, ejs);
 
 
 http.createServer(server).listen(port);
